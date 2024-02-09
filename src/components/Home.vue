2c57d2e423bc2a5c35756d9b1b307eb7aecc8e5a
devit2000 <ditway1405@gmail.com>
2024-02-09 11:50:56 +0700

    scroll bar

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,7 @@ import Person from "./icons/Person.vue";
   <main class="container flex justify-between items-center">
     <div class="text-[64px] font-bold">
       <h1>Hello</h1>
-      <h1>I'm Rithy Devit</h1>
+      <h1>I'm Rithy Devit,</h1>
       <h1 class="text-primary">Full-Stack Developer</h1>
       <button
         class="text-xl font-normal w-[201px] h-[50px] bg-primary text-black rounded-full"
