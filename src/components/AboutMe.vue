dce7cfe30ee6b596e66171d41cf4f9bdb243f7f0
devit2000 <ditway1405@gmail.com>
2024-02-10 16:00:58 +0700

    test with new animation

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -9,30 +9,65 @@ import Hobbie from "./shared/Hobbie.vue";
     <div
       class="flex flex-col-reverse lg:flex-row gap-10 justify-evenly items-center py-20"
     >
-      <div
-        class="block min-w-[300px] max-w-[700px]"
-        data-aos="fade-right"
-        data-aos-duration="500"
-        data-aos-anchor-placement="top-bottom"
-      >
-        <h1 class="text-primary text-2xl uppercase font-bold">
+      <div class="block min-w-[300px] max-w-[700px]">
+        <h1
+          class="text-primary text-2xl uppercase font-bold"
+          data-aos="fade-right"
+          data-aos-duration="300"
+          data-aos-anchor-placement="top-bottom"
+        >
           Full-Stack Developer
         </h1>
         <br />
-        <p class="font-light">
+        <p
+          class="font-light"
+          data-aos="fade-right"
+          data-aos-duration="400"
+          data-aos-anchor-placement="top-bottom"
+        >
           Management Information System student. I consider myself a responsible
           and orderly person. I am looking forward for my first work experience.
         </p>
         <br />
-        <Info title="fullname" text="Rithy Devit" />
-        <Info title="phone" text="+855 88 412 4798" />
-        <Info title="email" text="devitrithy1@gmail.com" />
-        <Info title="address" text="Ta Khmau, Kandal Cambodia" />
-        <Info title="language" text="Khmer, English" />
+        <Info
+          title="fullname"
+          text="Rithy Devit"
+          data-aos="fade-right"
+          data-aos-duration="500"
+          data-aos-anchor-placement="top-bottom"
+        />
+        <Info
+          title="phone"
+          text="+855 88 412 4798"
+          data-aos="fade-right"
+          data-aos-duration="600"
+          data-aos-anchor-placement="top-bottom"
+        />
+        <Info
+          title="email"
+          text="devitrithy1@gmail.com"
+          data-aos="fade-right"
+          data-aos-duration="700"
+          data-aos-anchor-placement="top-bottom"
+        />
+        <Info
+          title="address"
+          text="Ta Khmau, Kandal Cambodia"
+          data-aos="fade-right"
+          data-aos-duration="800"
+          data-aos-anchor-placement="top-bottom"
+        />
+        <Info
+          title="language"
+          text="Khmer, English"
+          data-aos="fade-right"
+          data-aos-duration="900"
+          data-aos-anchor-placement="top-bottom"
+        />
       </div>
       <div
         class="overflow-hidden rounded-xl border-primary shadow-border border-4"
-        data-aos="fade-left"
+        data-aos="zoom-in"
         data-aos-duration="500"
         data-aos-anchor-placement="top-bottom"
       >
